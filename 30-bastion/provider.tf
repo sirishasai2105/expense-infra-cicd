a37b83186a84751eeddfcbaebaf98239db33820e
siri123 <saisirishayarra692@gmail.com>
2024-11-26 15:22:20 +0530

    expense-infra

---
+++ 30-bastion/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.66.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "remote-state-bucket-aws"
+    key    = "siri-3"
+    region = "us-east-1"
+    dynamodb_table = "81s-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
